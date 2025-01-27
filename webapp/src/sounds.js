37ddf18f48651153cbc46a54383d560fc1c077da
maxlink <max.durham.link@gmail.com>
2025-01-27 09:48:52 -0600

    Enemy attack noise

--- webapp/src/sounds.js
+++ webapp/src/sounds.js
@@ -63,8 +63,60 @@ class Sounds {
     oscillator.stop(this.audioContext.currentTime + 1);
   }
 
- 
+  HitPlayer() {
+
+    this.initAudioContext();
+  
+      // Create a buffer for noise
+      const bufferSize = this.audioContext.sampleRate * 1; // 1 second of audio
+      const noiseBuffer = this.audioContext.createBuffer(1, bufferSize, this.audioContext.sampleRate);
+      const output = noiseBuffer.getChannelData(0);
+      
+      // Fill the buffer with white noise
+      for (let i = 0; i < bufferSize; i++) {
+          output[i] = Math.random() * 2 - 1; // Generate random values between -1 and 1
+      }
+  
+      const noiseSource = this.audioContext.createBufferSource();
+      noiseSource.buffer = noiseBuffer;
+  
+      // Add a low-pass filter for a more explosion-like sound
+      const filter = this.audioContext.createBiquadFilter();
+      filter.type = "lowpass";
+      filter.frequency.value = 1500; // Adjust frequency to change explosion timbre
+      filter.Q.value = 1;
+  
+      // Create a gain node for volume control
+      const gain = this.audioContext.createGain();
+      gain.gain.value = 0; // Start with gain at 0
+  
+      noiseSource.connect(filter);
+      filter.connect(gain);
+      gain.connect(this.audioContext.destination);
+  
+      function envelope(gainNode, time) {
+          const attackTime = 0.05;
+          const decayTime = 0.3;
+          const sustainLevel = 0.1;
+          const releaseTime = 0.4;
+  
+          gainNode.gain.setValueAtTime(1, time);
+          gainNode.gain.linearRampToValueAtTime(sustainLevel, time + attackTime);
+          gainNode.gain.exponentialRampToValueAtTime(0.001, time + attackTime + decayTime);
+          gainNode.gain.setValueAtTime(0, time + attackTime + decayTime + releaseTime);
+      }
+  
+      noiseSource.onended = () => {
+          this.isPlaying = false;
+      };
+  
+      noiseSource.start();
+      envelope(gain, this.audioContext.currentTime);
+      noiseSource.stop(this.audioContext.currentTime + 1);
+  }
 
 }
+
+
 
 const sounds = new Sounds()
