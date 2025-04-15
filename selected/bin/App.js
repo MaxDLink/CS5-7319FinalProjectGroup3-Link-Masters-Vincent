459c17d5cfe2dbfef320c679941b915eba03ef91
maxlink <max.durham.link@gmail.com>
2025-04-15 13:51:22 -0500

    update

--- selected/bin/App.js
+++ selected/bin/App.js
@@ -1,10 +1,8 @@
 "use strict";
-
 Object.defineProperty(exports, "__esModule", { value: true });
 require("source-map-support/register");
-
 const cdk = require("aws-cdk-lib");
 const App_1 = require("../lib/App");
 const app = new cdk.App();
-
-new App_1.App(app, 'App', {});
+new App_1.App(app, 'eventbus', {});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiQXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsdUNBQXFDO0FBQ3JDLG1DQUFtQztBQUNuQyxvQ0FBaUM7QUFFakMsTUFBTSxHQUFHLEdBQUcsSUFBSSxHQUFHLENBQUMsR0FBRyxFQUFFLENBQUM7QUFDMUIsSUFBSSxTQUFHLENBQUMsR0FBRyxFQUFFLFVBQVUsRUFBRSxFQUFFLENBQUMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnc291cmNlLW1hcC1zdXBwb3J0L3JlZ2lzdGVyJztcbmltcG9ydCAqIGFzIGNkayBmcm9tICdhd3MtY2RrLWxpYic7XG5pbXBvcnQgeyBBcHAgfSBmcm9tICcuLi9saWIvQXBwJztcblxuY29uc3QgYXBwID0gbmV3IGNkay5BcHAoKTtcbm5ldyBBcHAoYXBwLCAnZXZlbnRidXMnLCB7fSk7XG5cbiJdfQ==
